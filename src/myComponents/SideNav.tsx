1c7ba877a9991d757e85e79e41ffee9da6265e2b
Elmo0073! <nickbwylie@outlook.com>
2025-03-09 15:06:55 -0500

    updated the to make routing work

--- src/myComponents/SideNav.tsx
+++ src/myComponents/SideNav.tsx
@@ -1,4 +1,3 @@
-import { Progress } from "@/components/ui/progress";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import {
@@ -7,7 +6,6 @@ import {
   Book,
   HelpCircle,
   LogOut,
-  LayoutDashboard,
   ArrowLeftToLine,
   LogIn,
   MoreHorizontal,
